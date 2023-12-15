3354389ece25c5d753f5bdf8bc568fe8a75dad9f
naseeha <nesihafiss@gmail.com>
2023-12-15 17:02:00 +0530

    save

---
+++ webapp/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from . models import place
+def demo(request):
+    obj=place.objects.all()
+    return render(request,"index.html",{'result':obj})
+# Create your views here.
